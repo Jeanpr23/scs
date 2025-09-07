20437fc09850faf348913c6bd40748683ec6c489
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-07 18:01:12 -0400

    Add files via upload

---
+++ assignment/01_getting_started/01_01_python_org.py
@@ -0,0 +1,9 @@
+# Assignment 1-1: python.org
+
+# I visited the official Python website: https://www.python.org
+# The website provides the latest Python downloads for Windows, macOS, and Linux.
+# There is a section for documentation.
+# The community page has links to events and Python-related news.
+# I also found tutorials and beginner guides to help new learners start programming.
+# Overall, python.org is the central hub for resources, updates, and learning materials
+# related to the Python programming language.
