43bd53ba3dc58a6abf06e276a88c5c17c6ccf939
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-07 23:34:16 -0400

    Add files via upload

---
+++ assignment/02_variables_and_data_types/02_06_famous_quote2.py
@@ -0,0 +1,6 @@
+# Store a famous person's name in a variable and include it in the quote
+
+famous_person = "Albert Einstein"
+quote = "A person who never made a mistake never tried anything new."
+message = f'{'Albert Einstein'} once said, "{'A person who never made a mistake never tried anything new.'}'
+print('Albert Einstein once said, "A person who never made a mistake never tried anything new.')
