4b848c8ba6eb15ed8d3433d86ac6f2cdc1f0367e
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-07 22:06:14 -0400

    Add files via upload

---
+++ assignment/01_getting_started/01_03_infinite_skills.py
@@ -0,0 +1,8 @@
+# Assignment 1-3: Infinite Skills
+
+# Before this course, I have done some basic programming.
+# I have practiced small Python exercises, like printing messages and using variables.
+# I have also tried very simple projects like creating a calculator or a guessing game.
+# These experiences help me because I already know how to write code and test it.
+# I understand basic concepts like loops, conditionals, and functions.
+# This will help me in this course to learn faster and try more advanced exercises.
