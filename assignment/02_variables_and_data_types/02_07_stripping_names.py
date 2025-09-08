400b83250c647699fd8fc487e7737c469032a322
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-07 23:59:11 -0400

    Add files via upload

---
+++ assignment/02_variables_and_data_types/02_07_stripping_names.py
@@ -0,0 +1,7 @@
+# Use whitespace and strip it from a name
+
+name = "   Jean Paul   "
+print(f"Original: '{"Jean Paul"}'")
+print(f"Lstrip: '{"Jean Paul   "}'")
+print(f"Rstrip: '{"   Jean Paul"}'")
+print(f"Strip: '{"   Jean Paul   "}'")
