0ebb574303332715eb53c1998a4de16e1e2fed4e
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-11 12:34:18 -0400

    Add files via upload

---
+++ assignment/03_introducing_lists/# 03_01_names.py
@@ -0,0 +1,16 @@
+# 03_01_names.py
+
+# Me and my brothers names.
+
+names = ("Jean Paul", "Dylan", "David")
+print (names[0]+ ", Hey, Jean Paul welcome to Visual Studio Code")
+print (names[1]+ ", Hey, Dylan welcome to Visual Studio code")
+print (names[2]+ ", Hey, david welcome to Visual Studio code")
+
+# Extra coding I want to make for practice
+
+Persons = ("Jean Paul", "Dylan", "David")
+
+print (Persons[0]+", is 19 years old")
+print (Persons[1]+", is 17 years old")
+print (Persons[2]+", is 14 years old")
