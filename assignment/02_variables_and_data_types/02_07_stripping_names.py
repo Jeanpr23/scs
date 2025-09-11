fa8fea234118f2165d7fed4c57898ee8c4b79578
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-11 09:11:00 -0400

    Delete assignment/02_variables_and_data_types/02_07_stripping_names.py

--- assignment/02_variables_and_data_types/02_07_stripping_names.py
+++
@@ -1,7 +0,0 @@
-# Use whitespace and strip it from a name
-
-name = "   Jean Paul   "
-print(f"Original: '{"Jean Paul"}'")
-print(f"Lstrip: '{"Jean Paul   "}'")
-print(f"Rstrip: '{"   Jean Paul"}'")
-print(f"Strip: '{"   Jean Paul   "}'")
