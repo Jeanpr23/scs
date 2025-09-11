18436de5d11509e1e6f095d06382bbf7df898d12
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-11 09:10:28 -0400

    Add files via upload

---
+++ assignment/02_variables_and_data_types/02_07_stripping_names updated work.py
@@ -0,0 +1,8 @@
+# 02_07_stripping_names.py updated work 
+
+name = "   Jean Paul   "
+
+print(f"Original: '{name}'")
+print(f"Lstrip: {name.lstrip()}'")
+print(f"Rstrip: {name.rstrip()}'")
+print(f"Strip:  {name.strip()}'")
