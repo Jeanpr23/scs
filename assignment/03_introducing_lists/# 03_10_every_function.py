00f8469b2924f92e46770f3a4a49291e9949bb1e
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-15 22:26:02 -0400

    Add files via upload

---
+++ assignment/03_introducing_lists/# 03_10_every_function.py
@@ -0,0 +1,15 @@
+# 03_10_every_function.py
+
+favorites = ["Gaming", "Music", "Travel", "Foods", "Coding"]
+
+print(sorted(favorites))
+print(sorted(favorites, reverse=True))
+
+favorites.reverse()
+print(favorites)
+
+favorites.sort()
+print(favorites)
+
+favorites.sort(reverse=True)
+print(favorites)
