5597e530b98598d3ef0149b877e3de3390fa6f8b
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-17 22:21:36 -0400

    Delete assignment/03_introducing_lists/# 03_09_dinner_guests.py

--- assignment/03_introducing_lists/# 03_09_dinner_guests.py
+++
@@ -1,7 +0,0 @@
-# 03_09_dinner_guests.py
-
-# My guest list
-guests = ["David", "Dylan", "Raisa", "Pacman", "Mario", "Myke tyson"]
-
-# Print how many guests are invited
-print("Number of guests invited:", len(guests))
