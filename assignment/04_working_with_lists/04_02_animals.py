3967e5e383bfc0e75c194a7b630971b7a3121415
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-20 17:44:29 -0400

    Add files via upload

---
+++ assignment/04_working_with_lists/04_02_animals.py
@@ -0,0 +1,10 @@
+# 04_02_animals.py
+
+# list of animals that share a common trait
+animals = ["dog", "cat", "Turtle"]
+
+# loop to print the name of each animal
+for animal in animals:
+    print(animal)
+
+print("\nAny of these animals would make a great pet!")
