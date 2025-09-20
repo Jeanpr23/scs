fb301ede93e153e04405ee38d380f0693ddcad44
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-20 17:35:49 -0400

    Add files via upload

---
+++ assignment/04_working_with_lists/04_01_pizzas.py
@@ -0,0 +1,10 @@
+# 04_01_pizzas.py
+
+# list of favorite pizzas
+pizzas = ["pepperoni", "cheese", "bbq chicken"]
+
+# loop to print the names of the pizzas
+for pizza in pizzas:
+    print(pizza)
+
+print("\nI really love pizza!")
