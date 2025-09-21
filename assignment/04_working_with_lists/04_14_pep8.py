c821db1a3dad3bae9a24433eb0c3819e51c8d655
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-20 22:23:17 -0400

    Add files via upload

---
+++ assignment/04_working_with_lists/04_14_pep8.py
@@ -0,0 +1,10 @@
+# 04_14_pep8.py
+# PEP 8 is Python's style guide. It helps make code clean and easy to read.
+# This is the examples:
+#  for indentation
+# Keep lines short
+# Choose clear variable names
+# Add comments to explain code
+
+favorite_food = "pasta"
+print(f"My favorite food is {favorite_food}.")
