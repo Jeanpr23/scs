dc5b3360da21e11374015e98a8df21abf417e046
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-20 22:15:45 -0400

    Add files via upload

---
+++ assignment/04_working_with_lists/04_10_slice.py
@@ -0,0 +1,6 @@
+# 04_10_slice.py
+pizzas = ["pepperoni", "cheese", "bbq chicken", "veggie", "margherita"]
+
+print(pizzas[:3])   # first three
+print(pizzas[1:4])  # middle three
+print(pizzas[-3:])  # last three
