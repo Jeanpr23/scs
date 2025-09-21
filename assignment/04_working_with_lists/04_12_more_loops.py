b715d8e51c9c6fde3661d792fe3c9ab83d4d5f35
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-20 22:19:06 -0400

    Add files via upload

---
+++ assignment/04_working_with_lists/04_12_more_loops.py
@@ -0,0 +1,10 @@
+# 04_12_more_loops.py
+
+# List of animals
+animals = ["dog", "cat", "bird"]
+
+# a sentence about each animal
+for animal in animals:
+    print(f"A {animal} would make a great pet.")
+
+print("\nAll of these animals are great!")
