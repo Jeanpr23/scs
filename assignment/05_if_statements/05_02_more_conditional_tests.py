d4667f37de364a12bec1db8255c222eee7c34a74
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-29 19:56:27 -0400

    Add files via upload

---
+++ assignment/05_if_statements/05_02_more_conditional_tests.py
@@ -0,0 +1,13 @@
+# 05_02_more_conditional_tests.py
+
+game = 'The crew 2'
+print("Is game == 'The crew 2'? I predict True.")
+print(game == 'The crew 2') # True
+
+print("Is game == 'Apex leyends'? I predict False.")
+print(game == 'Apex leyends') # False
+
+
+card = 'Credit card'
+print("Is card == 'Credit card'? I predict True.")
+print(card == 'Credit card') # False
