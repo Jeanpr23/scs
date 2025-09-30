c97de1e9ec22bb8bfbf598752bac392032bfd583
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-29 20:57:21 -0400

    Add files via upload

---
+++ assignment/05_if_statements/05_12_deli.py
@@ -0,0 +1,8 @@
+# 05_12_deli.py
+
+sandwich_orders = ['tuna', 'pastrami', 'veggie']
+finished_sandwiches = []
+
+for sandwich in sandwich_orders:
+    print(f"I made your {sandwich} sandwich.")
+    finished_sandwiches.append(sandwich)
