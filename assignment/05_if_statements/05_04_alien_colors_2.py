47ed9f54f81d5d49f787affc52af790d6fd421a3
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-29 20:19:53 -0400

    Add files via upload

---
+++ assignment/05_if_statements/05_04_alien_colors_2.py
@@ -0,0 +1,8 @@
+# 05_04_alien_colors_2.py 
+
+alien_color = 'Blue'
+
+if alien_color == 'green': print("You earned 5 points!")
+else: print("You just earned 10 points!")
+
+if alien_color == 'Blue': print("You earned a lot of extra points!")
