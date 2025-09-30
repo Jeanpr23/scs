b9f10139aa61dac10e484689dce62428020110e7
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-09-29 20:38:44 -0400

    Add files via upload

---
+++ assignment/05_if_statements/05_07_favorite_fruit.py
@@ -0,0 +1,6 @@
+# 05_07_favorite_fruit.py
+
+favorites_fruits = ["'Banana, 'mango'"]
+
+if 'banana' in favorites_fruits: print("You really like bananas!")
+if 'mango' in favorites_fruits: print("You really love mangos!")
