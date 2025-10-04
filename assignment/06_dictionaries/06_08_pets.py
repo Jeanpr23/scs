7df947402bc26620196a735842fe3259962fbed5
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-04 10:09:39 -0400

    Add files via upload

---
+++ assignment/06_dictionaries/06_08_pets.py
@@ -0,0 +1,10 @@
+# 06_08_pets.py
+
+pet1 = {'animal': 'dog', 'owner': 'jeanpaul'}
+pet2 = {'animal': 'cat', 'owner': 'dylan'}
+pet3 = {'animal': 'parrot', 'owner': 'david'}
+
+pets = [pet1, pet2, pet3]
+
+for pet in pets:
+    print(pet)
