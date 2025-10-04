1a4d5432d448ddd4f09a6363d1b5af932542f121
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-04 09:51:35 -0400

    Add files via upload

---
+++ assignment/06_dictionaries/06_01_person.py
@@ -0,0 +1,9 @@
+# 06_01_person.py
+
+favorite_numbers = {
+    'Jean Paul': 21,
+    'Batman': 40,
+}
+
+for name, number in favorite_numbers.items():
+    print(f"{name}'s favorite number is {number}.")
