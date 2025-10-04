88bb9ad563105d5077aebdadb4609518dda8c1c2
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-04 10:07:46 -0400

    Add files via upload

---
+++ assignment/06_dictionaries/06_07_people.py
@@ -0,0 +1,13 @@
+# 06_07_people.py
+
+
+person1 = {'first_name': 'jeanpaul', 'last_name': 'aponte santiago', 'age': 19, 'city': 'reading'}
+person2 = {'first_name': 'dylan', 'last_name': 'aponte santiago', 'age': 17, 'city': 'reading'}
+person3 = {'first_name': 'david', 'last_name': 'aponte santiago', 'age': 14, 'city': 'reading'}
+
+
+people = [person1, person2, person3]
+
+
+for person in people:
+    print(person)
