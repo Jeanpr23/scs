7a2752f68d711836b304a134af2944ade69a5bba
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-04 09:55:38 -0400

    Add files via upload

---
+++ assignment/06_dictionaries/06_03_glossary.py
@@ -0,0 +1,12 @@
+# 06_03_glossary.py
+
+glossary = {
+    'variable': 'A name that stores a value.',
+    'function': 'A block of code that does a task.',
+    'loop': 'Repeats a block of code.',
+    'dictionary': 'Stores key value pairs.',
+    'list': 'An ordered collection of items.',
+}
+
+for word, definition in glossary.items():
+    print(word, ":", definition)
