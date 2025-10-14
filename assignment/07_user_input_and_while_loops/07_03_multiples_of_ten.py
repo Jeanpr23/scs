8a819427f928ed111fe076bf6deed48e4c7de62c
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-14 09:12:42 -0400

    Add files via upload

---
+++ assignment/07_user_input_and_while_loops/07_03_multiples_of_ten.py
@@ -0,0 +1,8 @@
+# 07_03_multiples_of_ten.py
+
+number = int(input("Enter a number: "))
+
+if number % 10 == 0:
+    print("That number is a multiple of 10.")
+else:
+    print("That number is not a multiple of 10.")
