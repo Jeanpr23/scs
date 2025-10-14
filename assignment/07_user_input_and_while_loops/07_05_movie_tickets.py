8cf8e4c211c862ff8be139b8bfc9d44d43ed9ba4
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-14 09:15:18 -0400

    Add files via upload

---
+++ assignment/07_user_input_and_while_loops/07_05_movie_tickets.py
@@ -0,0 +1,15 @@
+# 07_05_movie_tickets.py
+
+while True:
+    age = input("Enter your age (or 'quit' to stop): ")
+    if age.lower() == 'quit':
+        break
+
+    age = int(age)
+
+    if age < 3:
+        print("Your ticket is free!")
+    elif age <= 12:
+        print("Your ticket is $10.")
+    else:
+        print("Your ticket is $15.")
