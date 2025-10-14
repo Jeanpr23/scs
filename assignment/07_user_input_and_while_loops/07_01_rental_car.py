33831c34ccb47ba9d6f0567731acf6c6931b97e5
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-14 09:10:04 -0400

    Add files via upload

---
+++ assignment/07_user_input_and_while_loops/07_01_rental_car.py
@@ -0,0 +1,7 @@
+# 07_01_rental_car.py
+
+# Ask user what kind of car they want
+car = input("What kind of rental car would you like? ")
+
+# Simple confirmation message
+print(f"Let me see if I can find you a {car}.")
