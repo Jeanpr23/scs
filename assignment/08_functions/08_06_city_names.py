2c15b07c0204093d96a9f31bb2cc71f73255a2e8
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-20 19:23:29 -0400

    Add files via upload

---
+++ assignment/08_functions/08_06_city_names.py
@@ -0,0 +1,9 @@
+# 08_06_city_names.py
+
+def city_country(city, country):
+    """Returns a formatted string with the city and country."""
+    return f"{city}, {country}"
+
+print(city_country("San Juan", "Puerto Rico"))
+print(city_country("Paris", "France"))
+print(city_country("Tokyo", "Japan"))
