5ff9ba7c58bfdf50630702c29cd826885455c61a
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-20 18:22:37 -0400

    Add files via upload

---
+++ assignment/08_functions/08_03_tshirt.py
@@ -0,0 +1,7 @@
+# 08_03_tshirt.py
+
+def make_tshirt(size, message):
+    """Prints a summary of the T-shirt being made."""
+    print(f"Making a {size} T-shirt with the message: '{message}'.")
+
+make_tshirt("Medium", "Python is powerful!")
