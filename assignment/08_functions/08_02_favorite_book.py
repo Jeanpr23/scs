2af024aae99642d79f0c6a10208cf607b6717cbb
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-20 18:18:53 -0400

    Add files via upload

---
+++ assignment/08_functions/08_02_favorite_book.py
@@ -0,0 +1,7 @@
+# 08_02_favorite_book.py
+
+def favorite_book(title):
+    """a message about a favorite book."""
+    print(f"One of my favorite books is '{title}'.")
+
+favorite_book("Life on Delay: Making Peace with a Stutter")
