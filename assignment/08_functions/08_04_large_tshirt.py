8654e3a9df0e84e64f743370766d65d22999b62e
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-20 18:33:30 -0400

    Add files via upload

---
+++ assignment/08_functions/08_04_large_tshirt.py
@@ -0,0 +1,8 @@
+# 08_04_large_tshirt.py
+
+def make_tshirt(size="Large", message="I love Python"):
+    """Prints a summary of the T-shirt being made, using default values."""
+    print(f"Making a {size} T-shirt with the message: '{message}'.")
+
+make_tshirt()
+make_tshirt(message="Code like a pro")
