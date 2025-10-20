5b7a3ba78908dab3a148408d4ac075a8deb09f38
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-20 18:44:03 -0400

    Add files via upload

---
+++ assignment/08_functions/08_05_cities.py
@@ -0,0 +1,9 @@
+# 08_05_cities.py
+
+def describe_city(city, country="USA"):
+    """Prints a message describing the location of a city."""
+    print(f"{city} is in {country}.")
+
+describe_city("Reading")
+describe_city("Barcelona", "Spain")
+describe_city("Tokyo", "Japan")
