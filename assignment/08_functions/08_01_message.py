a42e8f00a4c46a8e36376fffdf0a5b74643a9730
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-20 18:14:15 -0400

    Add files via upload

---
+++ assignment/08_functions/08_01_message.py
@@ -0,0 +1,7 @@
+# 08_01_message.py
+
+def display_message():
+    """Prints a message about what you're learning in this course."""
+    print("I'm learning how to write functions in Python!")
+
+display_message()
