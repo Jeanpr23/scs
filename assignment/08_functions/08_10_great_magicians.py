04bf5a7f984c2340a730f7dbd4493458b0d5c43b
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-21 08:37:30 -0400

    Add files via upload

---
+++ assignment/08_functions/08_10_great_magicians.py
@@ -0,0 +1,11 @@
+# 08_10_great_magicians.py
+
+def show_magicians(magicians):
+    """Prints the name of each magician in the list."""
+    for magician in magicians:
+        print(magician)
+
+# List of magician names
+magician_names = ["Houdini", "David Blaine", "Criss Angel", "Dynamo"]
+
+show_magicians(magician_names)
