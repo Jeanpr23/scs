25cf53a3f01da64e57a15f35fdb1be7df1a0be99
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-10-21 08:35:59 -0400

    Add files via upload

---
+++ assignment/08_functions/08_08_user_albums.py
@@ -0,0 +1,22 @@
+# 08_08_user_albums.py
+
+def make_album(artist, title):
+    """Returns a dictionary with album information."""
+    return {
+        "artist": artist,
+        "title": title
+    }
+
+while True:
+    print("\nEnter album information (or type 'q' to quit):")
+
+    artist_name = input("Artist name: ")
+    if artist_name.lower() == 'q':
+        break
+
+    album_title = input("Album title: ")
+    if album_title.lower() == 'q':
+        break
+
+    album = make_album(artist_name, album_title)
+    print(f"Album created: {album}")
