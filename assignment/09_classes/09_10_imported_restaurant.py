e3529290f9ec90c4f6632ad40887427845ced69d
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-04 11:12:09 -0500

    Add files via upload

---
+++ assignment/09_classes/09_10_imported_restaurant.py
@@ -0,0 +1,7 @@
+# 09_10_imported_restaurant.py
+
+from restaurant import Restaurant  
+
+my_restaurant = Restaurant("Pizza World", "Italian")
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
