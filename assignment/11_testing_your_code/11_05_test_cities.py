cf828caa111b7ca49117c49ec16a6a444d4bd0b5
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-04 12:03:10 -0500

    Add files via upload

---
+++ assignment/11_testing_your_code/11_05_test_cities.py
@@ -0,0 +1,7 @@
+# 11_05_test_cities.py
+
+from city_functions import city_country  # Make sure city_functions.py exists
+
+print(city_country("paris", "france"))
+print(city_country("tokyo", "japan"))
+print(city_country("sydney", "australia"))
