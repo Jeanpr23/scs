a9220fa00b3c4292368c01f3bb1525f59b624805
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-04 11:55:52 -0500

    Add files via upload

---
+++ assignment/11_testing_your_code/11_01_city_country.py
@@ -0,0 +1,8 @@
+# 11_01_city_country.py
+
+def city_country(city, country):
+    return f"{city.title()}, {country.title()}"
+
+print(city_country("paris", "france"))
+print(city_country("tokyo", "japan"))
+print(city_country("sydney", "australia"))
