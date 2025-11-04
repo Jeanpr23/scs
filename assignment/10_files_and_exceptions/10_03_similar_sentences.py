efa755ad64c21e802d6aacc53ed3228af36dd137
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-04 11:38:24 -0500

    Add files via upload

---
+++ assignment/10_files_and_exceptions/10_03_similar_sentences.py
@@ -0,0 +1,7 @@
+# 10_03_similar_sentences.py
+
+languages = ["python", "c", "java", "javascript"]
+
+for language in languages:
+    print(f"I like learning {language.title()}.")
+    print(f"{language.title()} is fun to practice!\n")
