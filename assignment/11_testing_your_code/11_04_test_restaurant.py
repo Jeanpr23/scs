a1fed91875949c60049fc06a457bcf748655903f
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-04 11:59:44 -0500

    Add files via upload

---
+++ assignment/11_testing_your_code/11_04_test_restaurant.py
@@ -0,0 +1,7 @@
+# 11_04_test_restaurant.py
+
+from restaurant import Restaurant  # Make sure restaurant.py exists
+
+my_restaurant = Restaurant("Pizza World", "Italian")
+my_restaurant.describe_restaurant()
+my_restaurant.open_restaurant()
