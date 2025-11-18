bcb97ea811d29c7b11c9f463d12fefa51d8a58c5
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-17 23:46:00 -0500

    Add files via upload

---
+++ assignment/12_a_ship_that_fires_bullets/12_05_keys.py
@@ -0,0 +1,13 @@
+student = {
+    "name": "Jean Paul",
+    "age": 19,
+    "major": "Game and sim"
+}
+
+# Print all keys
+print("Dictionary keys:")
+print(student.keys())
+
+# acess 
+print("\nStudent name:")
+print(student["name"])
