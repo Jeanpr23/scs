fa5824e29fe1fc2be7d84c64cfedda3877b938ff
Jean Paul <jeanpauldavidaponte@gmail.com>
2025-11-18 00:01:17 -0500

    Add files via upload

---
+++ assignment/12_a_ship_that_fires_bullets/12_19_refactor.py
@@ -0,0 +1,11 @@
+# 12_19_refactor.py
+
+
+def print_star_line(spaces, stars):
+    print(" " * spaces + "*" * stars)
+
+
+print_star_line(3, 1)
+print_star_line(2, 3)
+print_star_line(1, 5)
+print_star_line(0, 7)
